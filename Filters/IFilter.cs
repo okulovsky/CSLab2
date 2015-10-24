4be67c5036883a4f3edb1221c603f483fe2a8914
okulovsky <yuri.okulovsky@gmail.com>
2015-10-24 09:15:40 +0500

    ParameterInfo in a separate file

--- Filters/IFilter.cs
+++ Filters/IFilter.cs
@@ -2,14 +2,7 @@ using System;
 
 namespace MyPhotoshop
 {
-	public class ParameterInfo
-	{
-		public string Name;
-		public double DefaultValue;
-		public double MinValue=0;
-		public double MaxValue=1;
-		public double Increment;
-	}
+
 	
 	public interface IFilter
 	{
