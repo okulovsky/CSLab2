3ac63c095e38a06121d3e162c09dde854e11ab17
okulovsky <yuri.okulovsky@gmail.com>
2015-12-10 15:32:18 +0600

    TransformFilter

---
+++ Filters/TransformFilter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace MyPhotoshop
+{
+    public class TransformFilter : ParametrizedFilter<EmptyParameters>
+    {
+        public Func<Size, Size> sizeTransform;
+        public Func<Size, Point, Point> pointTransform;
+        string name;
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+        public TransformFilter(string name, Func<Size, Size> sizeTransform, Func<Size, Point, Point> pointTransform)
+        {
+            this.sizeTransform = sizeTransform;
+            this.pointTransform = pointTransform;
+            this.name = name;
+        }
+
+        public override Photo Process(Photo original, EmptyParameters parameters)
+        {
+            var oldSize = new Size(original.Width, original.Height);
+            var newSize = sizeTransform(oldSize);
+            var result = new Photo(newSize.Width, newSize.Height);
+            for (int x=0;x<newSize.Width;x++)
+                for (int y = 0; y < newSize.Height; y++)
+                {
+                    var oldPoint = pointTransform(oldSize, new Point(x, y));
+                    result[x, y] = original[oldPoint.X, oldPoint.Y];
+                }
+            return result;
+        }
+    }
+}
--- Main.cs
+++ Main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace MyPhotoshop
@@ -10,16 +11,33 @@ namespace MyPhotoshop
 		{
 			var window=new MainWindow();
 
-            var lighteningFilter = new PixelFilter<LighteningParameters>("Осветление/затемнение", (pixel,parameters)=>pixel*parameters.Coefficient);
+
+
+
+            var mirrorHorizontal = new TransformFilter("Отразить по горизонтали",
+                size => size,
+                (oldSize, point) => new Point(oldSize.Width - point.X-1, point.Y)
+                );
+            window.AddFilter(mirrorHorizontal);
+
+            var rotateClockwise = new TransformFilter("Повернуть против ч.с.",
+                size => new Size(size.Height, size.Width),
+                (oldSize, point) => new Point(point.Y, point.X)
+                );
+            window.AddFilter(rotateClockwise);
+
+            var lighteningFilter = new PixelFilter<LighteningParameters>("Осветление/затемнение", (pixel, parameters) => pixel * parameters.Coefficient);
             window.AddFilter(lighteningFilter);
 
-            var grayscaleFilter = new PixelFilter<EmptyParameters>("Оттенки серого", 
-                (original,parameters)=>
+            var grayscaleFilter = new PixelFilter<EmptyParameters>("Оттенки серого",
+                (original, parameters) =>
                 {
                     var gray = 0.299 * original.R + 0.587 * original.G + 0.114 * original.B;
                     return new Pixel(gray, gray, gray);
                 });
             window.AddFilter(grayscaleFilter);
+
+
 			Application.Run (window);
 		}
 	}
